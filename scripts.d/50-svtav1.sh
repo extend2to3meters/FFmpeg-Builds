010e3a105990a13ac886636274f7199540ae5281
extend2to3meters <45506051+extend2to3meters@users.noreply.github.com>
2022-01-20 23:15:38 +0800

    Update 50-svtav1.sh

--- scripts.d/50-svtav1.sh
+++ scripts.d/50-svtav1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SVTAV1_REPO="https://gitlab.com/AOMediaCodec/SVT-AV1.git"
-SVTAV1_COMMIT="3758f57f756fa2f6721460ed7b2db2ac8a5f44e0"
+SVTAV1_COMMIT="97f4d66d4abf41550ad5781d88d35d2dd6f90fda"
 
 ffbuild_enabled() {
     [[ $TARGET == win32 ]] && return -1
